0a9af2e171c12c50ac97e6bf82e8eca8d7d27c3d
ValentinaNovarino <valenovarino@gmail.com>
2020-12-02 09:41:21 +0100

    menu footer

--- main.js
+++ main.js
@@ -2,10 +2,20 @@ var app = new Vue (
     {
         el: '#root',
         data: {
+            menuItems: [
+                'Home',
+                'Meet The Band',
+                'Live Dates',
+                'Latest News',
+                'Albums',
+                'Fans'
+            ]
 
         },
         methods: {
+            showMenu() {
 
+            }
         }
     }
 );
