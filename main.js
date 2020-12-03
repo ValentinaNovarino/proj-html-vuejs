0d80ca86b898c1e7c006269a9e1a1f1f38b07f2d
ValentinaNovarino <valenovarino@gmail.com>
2020-12-03 14:00:23 +0100

    live-dates dinamico

--- main.js
+++ main.js
@@ -10,14 +10,38 @@ var app = new Vue (
                 'Albums',
                 'Fans'
             ],
-            showMenu: false,
+            isVisible: true,
+            lives: [
+                {
+                    date: '17/08/2020',
+                    name: 'gem festival',
+                    place: 'anakalia, georgia'
+                },
+                {
+                    date: '24/09/2020',
+                    name: 'groovefest',
+                    place: 'dominical republic'
+                },
+                {
+                    date: '31/10/2020',
+                    name: 'oasis festival -',
+                    place: 'marrakech, marocco'
+                },
+                {
+                    date: '7/11/2020',
+                    name: 'moga festival -',
+                    place: 'essaouria, marocco'
+                },
+                {
+                    date: '10/12/2020',
+                    name: 'envision festival -',
+                    place: 'uvita, costa rica'
+                }
+            ]
         },
         methods: {
-            showDropdownMenu() {
-                this.showMenu = true;
-            },
-            closeDropdownMenu() {
-                this.showMenu = false;
+            openCloseDropdownMenu() {
+                this.isVisible = !this.isVisible;
             }
         }
     }
